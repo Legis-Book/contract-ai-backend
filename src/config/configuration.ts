1b54625f599a1e8cafd35959666bbe24ce2dbc16
Chintan Shah <chintanshah@promactinfo.com>
2025-05-30 15:04:01 +0530

    refactor(config): update AWS S3 configuration environment variable names for consistency and clarity; add new entries to VSCode settings

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -54,9 +54,9 @@ export default () => ({
   },
   file: {
     awsS3Region: process.env.AWS_S3_REGION || '',
-    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID || '',
-    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY || '',
-    awsDefaultS3Bucket: process.env.AWS_S3_BUCKET || '',
+    accessKeyId: process.env.ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.SECRET_ACCESS_KEY || '',
+    awsDefaultS3Bucket: process.env.AWS_DEFAULT_S3_BUCKET || '',
     maxFileSize: parseInt(process.env.MAX_FILE_SIZE || '10000000', 10),
   },
 });
